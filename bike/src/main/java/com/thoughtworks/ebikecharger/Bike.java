602b32540515983b49423a160868ed7d483cdf05
FanJingjia <jingjia.fan@thoughtworks.com>
2022-12-16 16:49:13 +0800

    feat: Bike构造器添加初始化请求，用于将bike的初始数据发送给Server

--- bike/src/main/java/com/thoughtworks/ebikecharger/Bike.java
+++ bike/src/main/java/com/thoughtworks/ebikecharger/Bike.java
@@ -1,5 +1,15 @@
 package com.thoughtworks.ebikecharger;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+
 public class Bike {
 
   private String id;
@@ -10,17 +20,31 @@ public class Bike {
   // 使用者名称=使用者名称/空字符串
   private String username;
 
+  private ObjectMapper mapper = new ObjectMapper();
+
   public Bike(String id) {
     this.id = id;
+    this.status = "plugIn";
+    this.username = "";
+    try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+      HttpPost httpPost = new HttpPost("http://127.0.0.1:9999/bike/init");
+      Map<String, Bike> params = new HashMap<>();
+      params.put("bike", this);
+      String paramsJson = mapper.writeValueAsString(params);
+      httpPost.setEntity(new StringEntity(paramsJson, ContentType.APPLICATION_JSON));
+      httpClient.execute(httpPost);
+      httpPost.clear();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   public void plugIn() {
     setStatus("plugIn");
   }
 
-  public void plugOut(String username) {
+  public void plugOut() {
     setStatus("plugOut");
-    setUsername(username);
   }
 
   public String getId() {
@@ -29,9 +53,5 @@ public class Bike {
 
   public void setStatus(String status) {
     this.status = status;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
   }
 }
