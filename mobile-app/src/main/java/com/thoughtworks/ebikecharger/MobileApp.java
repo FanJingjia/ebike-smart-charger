3544f61b820a327ad34962009b8e1068b5d7b05b
FanJingjia <jingjia.fan@thoughtworks.com>
2022-12-16 17:18:27 +0800

    feat: MobileApp的获取bike接口新增bikeId参数，用于确定获取的bike

--- mobile-app/src/main/java/com/thoughtworks/ebikecharger/MobileApp.java
+++ mobile-app/src/main/java/com/thoughtworks/ebikecharger/MobileApp.java
@@ -2,11 +2,14 @@ package com.thoughtworks.ebikecharger;
 
 import static com.thoughtworks.ebikecharger.Constants.HTTP_STATUS_OK;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
@@ -19,6 +22,8 @@ import org.apache.hc.core5.http.io.entity.StringEntity;
 public class MobileApp {
 
   private final String username;
+
+  private final ObjectMapper mapper = new ObjectMapper();
 
   public MobileApp(String username) {
     this.username = username;
@@ -34,7 +39,7 @@ public class MobileApp {
       HttpGet httpGet = new HttpGet("http://127.0.0.1:9999/bike/status");
       CloseableHttpResponse response = httpClient.execute(httpGet);
 
-      if (response.getCode()==HTTP_STATUS_OK){
+      if (response.getCode() == HTTP_STATUS_OK) {
         HttpEntity entity = response.getEntity();
         InputStream contentInputStream = entity.getContent();
         BufferedReader reader = new BufferedReader(new InputStreamReader(contentInputStream, StandardCharsets.UTF_8));
@@ -53,12 +58,15 @@ public class MobileApp {
   }
 
   // 请求修改bike的状态
-  public void reportBorrower() {
-
+  public void reportBorrower(String bikeId) {
     synchronized (MobileApp.class) {
       try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
         HttpPost httpPost = new HttpPost("http://127.0.0.1:9999/bike/status");
-        httpPost.setEntity(new StringEntity(username, StandardCharsets.UTF_8));
+        Map<String, String> params = new HashMap<>();
+        params.put("username", username);
+        params.put("bikeId", bikeId);
+        String paramsJson = mapper.writeValueAsString(params);
+        httpPost.setEntity(new StringEntity(paramsJson));
         httpClient.execute(httpPost);
         httpPost.clear();
       } catch (IOException e) {
